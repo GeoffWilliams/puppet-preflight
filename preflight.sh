05256e1c4733db41da9faffaee2ed8c06235a2b8
Geoff Williams <geoff.williams@puppetlabs.com>
2015-05-29 16:25:40 +1000

    check for missing libcrypt.so

--- preflight.sh
+++ preflight.sh
@@ -4,7 +4,7 @@
 CLEAN=true
 
 # The last version this tool was tested against
-PE_RELEASE="3.7.2"
+PE_RELEASE="3.8.0"
 
 PE_DIR="/opt/puppet"
 PE_CONF_DIR="/etc/puppetlabs"
@@ -79,6 +79,13 @@ if [ $(stat -c '%a' /var/log) != "755" ] ; then
   CLEAN=false
 fi
 
+# BUGS PE-9506, SERVER-592, PE-9973
+# Jruby doesn't work on Debian/Ubuntu systems when libcrypt.so is not in the
+# default library path
+if [ ! -e "/usr/lib64" ] ; then
+  echo "PE-9506, SERVER-592, PE-9973 -- please ensure libcrypt.so available under /usr/lib64, eg: ln -s /lib/x86_64-linux-gnu/ /usr/lib64"
+  CLEAN=false
+fi
 
 # Overall status
 if [ $CLEAN = true ] ; then
